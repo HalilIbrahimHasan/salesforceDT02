2819207c01b1de5a48aae9ea38a227f6b251864f
Ibrahim Kalin <hisetkal@gmail.com>
2022-06-02 10:44:56 -0400

    first commit

---
+++ src/firstpackage/FirstClass.java
@@ -0,0 +1,12 @@
+package firstpackage;
+
+public class FirstClass {
+
+    //Welcome everyone to day 2 of Git
+    //Hello dear developers, did you join?
+
+    //Hello dear developers, did you join?
+    //yes we did
+
+
+}
